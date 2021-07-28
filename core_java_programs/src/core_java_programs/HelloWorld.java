1af00af3c55d94aa9297a95956173b881a0ed931
reachgguru <reachgguru.gmail.com>
2021-07-28 20:03:06 +0530

    First commit

---
+++ core_java_programs/src/core_java_programs/HelloWorld.java
@@ -0,0 +1,10 @@
+package core_java_programs;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first program");
+	}
+
+}
